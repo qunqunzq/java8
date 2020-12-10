c63775dbee28f28e93f8e8e7e2b7fc87ca378ea6
qun <qunqunzq@126.com>
2020-12-10 18:20:57 +0800

    Revert "111"
    dev

--- src/main/java/com/qunzq/java8/dao/Java8.java
+++ src/main/java/com/qunzq/java8/dao/Java8.java
@@ -20,6 +20,6 @@ public class Java8 {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("count",list);
         System.out.println(jsonObject.toString());
-
+        System.out.println("dev");
     }
 }
